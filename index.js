5d461a6872bfe9d9460fd68570e622be44ce292e
Ranjeet Kumar Yadav <innovativeranjeet@gmail.com>
2023-08-22 09:57:44 -0400

    two middleware with productpage

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const http = require('http');
+const bodyParser=require('body-parser');
 
 const express = require('express');
 //const { create } = require('domain');
 const index= express();
 
+index.use(bodyParser.urlencoded({extended: false}));
 
 index.use('/Add-product',(req,res,next)=>{
     //console.log('In another middleware');
     res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="Enter your product name"><input type="number" name="Size" placeholder="enter the size of product"><button type="submit" >Add-Product</button></form>');
  })
+
+ index.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+ });
 
 /*index.use((req,res,next)=>{
     console.log('In the middleware');
@@ -19,6 +26,8 @@ index.use('/',(req,res,next)=>{
    // console.log('In another middleware');
     res.send('<h1>Welcome to our country</h1>');
 })
+
+
 
 const server = http.createServer(index);
 
