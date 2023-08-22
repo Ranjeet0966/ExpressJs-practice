9913e1aab442fe7e6a0b887050dc839dd41c02ad
Ranjeet Kumar Yadav <innovativeranjeet@gmail.com>
2023-08-22 09:56:42 -0400

    adding two middleware and create form for product

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const express = require('express');
 //const { create } = require('domain');
 const index= express();
 
-index.use((req,res,next)=>{
+
+index.use('/Add-product',(req,res,next)=>{
+    //console.log('In another middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="Enter your product name"><input type="number" name="Size" placeholder="enter the size of product"><button type="submit" >Add-Product</button></form>');
+ })
+
+/*index.use((req,res,next)=>{
     console.log('In the middleware');
     next();//Allows the request to continue to the next middleware 
-})
+})*/
 
-index.use((req,res,next)=>{
-    console.log('In another middleware');
+index.use('/',(req,res,next)=>{
+   // console.log('In another middleware');
     res.send('<h1>Welcome to our country</h1>');
 })
 
